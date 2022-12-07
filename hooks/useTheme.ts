d2f18ffb5bae77163ba30b6ed1885d4e78190229
Yourim Cha <yourim.cha@navercorp.com>
2022-12-07 17:56:37 +0900

    Add darkmode theme

---
+++ hooks/useTheme.ts
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+
+export type ThemeOption = 'system' | 'light' | 'dark';
+export function useTheme(initialTheme?: ThemeOption) {
+  const [theme, setTheme] = useState<ThemeOption | undefined>(initialTheme);
+
+  useEffect(() => {
+    const mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)');
+    const handleSystemThemeChange = (e: MediaQueryListEvent) => {
+      if (e.matches) {
+        window.document.body.classList.add('darkmode');
+      } else {
+        window.document.body.classList.remove('darkmode');
+      }
+    };
+
+    if (theme === 'dark') {
+      window.document.body.classList.add('darkmode');
+      window.localStorage.setItem('theme', 'dark');
+    } else if (theme === 'light') {
+      window.document.body.classList.remove('darkmode');
+      window.localStorage.setItem('theme', 'light');
+    } else if (theme === 'system') {
+      window.localStorage.setItem('theme', 'system');
+      if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        window.document.body.classList.add('darkmode');
+      }
+      mediaQueryList.addEventListener('change', handleSystemThemeChange);
+    }
+    return () => {
+      mediaQueryList.removeEventListener('change', handleSystemThemeChange);
+    };
+  }, [theme]);
+
+  return { setTheme };
+}
