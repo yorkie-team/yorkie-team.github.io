4f0dd04014c1d48de82035b39d32cc78eda68bab
Kim Dong Hun <hunkim98@gmail.com>
2022-12-16 21:15:19 +0900

    Change kanban doc structure

--- components/BasicExampleProjects/kanban/documentStructure.ts
+++ components/BasicExampleProjects/kanban/documentStructure.ts
@@ -1,13 +1,13 @@
 export const KanbanDocumentStructure = `export type KanbanDoc = {
-lists: Array<List>;
+    lists: Array<List>;
 };
 
 export type List = {
-title: string;
-cards: Array<Card>;
+    title: string;
+    cards: Array<Card>;
 };
 
 export type Card = {
-title: string;
+    title: string;
 };
 `;
