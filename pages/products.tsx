988d150bf2371405f7f2a69ef92499a943b37ef0
Kevin Park <wlghks0314@gmail.com>
2023-01-13 20:08:44 +0900

    Fix CLI page link in main page & mobile dropdown (#51)

--- pages/products.tsx
+++ pages/products.tsx
@@ -191,7 +191,7 @@ const Products: NextPage = () => {
               <li className="product_card_item">
                 <strong className="product_card_title">Security</strong>
                 <p className="product_card_desc">
-                  <Link href="/docs/project#auth-webhook" className="link">
+                  <Link href="/docs/cli#auth-webhook" className="link">
                     Auth Webhook
                   </Link>{' '}
                   allows users to verify the authorization of clients to access documents from an external service.
