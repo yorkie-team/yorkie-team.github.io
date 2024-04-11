27d8aee77d5ea0616d65ece9b797def75e1a1dc0
DongjaJ <jdh981118@naver.com>
2024-04-11 20:29:42 +0900

    fix: remove output property and update env property in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
   images: {
@@ -11,9 +10,9 @@ const nextConfig = {
   env: {
     nextImageExportOptimizer_imageFolderPath: 'public/assets/images',
     nextImageExportOptimizer_exportFolderPath: 'out',
-    nextImageExportOptimizer_quality: 75,
-    nextImageExportOptimizer_storePicturesInWEBP: true,
-    nextImageExportOptimizer_generateAndUseBlurImages: true,
+    nextImageExportOptimizer_quality: '75',
+    nextImageExportOptimizer_storePicturesInWEBP: 'true',
+    nextImageExportOptimizer_generateAndUseBlurImages: 'true',
     NEXT_PUBLIC_BUILT_AT: new Date().toUTCString(),
   },
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
