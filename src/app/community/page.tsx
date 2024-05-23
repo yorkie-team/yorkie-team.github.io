e2df7a77dc721c486090c850edf4467b06238a85
Vi Tran ( Alice) <vi.tran@navercorp.com>
2024-05-23 18:21:29 +0700

    update code community

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -17,48 +17,59 @@ const Community: NextPage = () => {
         margin="auto"
         width={{ sm: 'breakpoint-sm', md: 'breakpoint-md', lg: 'breakpoint-lg', xl: 'breakpoint-xl' }}
       >
-        <Flex justifyContent="space-between">
-          <Box>
-            <Heading as="h2" fontSize="6xl" color="black.a9">
-              Join our community
-            </Heading>
-            <Text fontWeight="semibold" paddingBlock="10" lineHeight="normal">
-              If you have any questions along the way,
-              <br className="br_mo" /> please don’t hesitate to ask us
-              <br className="br_tablet" /> through our
-              <br className="br_mo" />
-              <br className="br_pc" />
-              channels.
-              <br className="br_mo_xs" /> You can sign up for our Discord or
-              <br className="br_mo" /> raise GitHub
-              <br className="br_tablet" /> discussions.
-            </Text>
-            <Flex gap="6">
-              <Button
-                icon={<DiscordIcon />}
-                href="https://discord.gg/MVEAwz9sBy"
-                as="link"
-                variant="outline"
-                position="start"
-                size="lg"
-              >
-                Discord
-              </Button>
-              <Button
-                icon={<GitHubIcon />}
-                href="https://github.com/yorkie-team/dashboard/issues"
-                as="link"
-                variant="outline"
-                position="start"
-                size="lg"
-              >
-                GitHub
-              </Button>
-            </Flex>
-          </Box>
-          <Box>
-            <CommunitySVG />
-          </Box>
+        <Flex
+          justifyContent="space-between"
+          flexDirection="column"
+          paddingInline={{ base: '6', lg: '0' }}
+          paddingBlock={{ base: '12', lg: '40' }}
+        >
+          <Heading as="h2" fontSize={{ base: '3xl', lg: '6xl' }} color="black.a9">
+            Join our community
+          </Heading>
+          <Flex
+            justifyContent="space-between"
+            alignItems="center"
+            flexDirection={{ base: 'column-reverse', lg: 'row' }}
+          >
+            <Box>
+              <Text fontWeight="semibold" paddingBottom="10" lineHeight="normal">
+                If you have any questions along the way,
+                <br className="br_mo" /> please don’t hesitate to ask us
+                <br className="br_tablet" /> through our
+                <br className="br_mo" />
+                <br className="br_pc" />
+                channels.
+                <br className="br_mo_xs" /> You can sign up for our Discord or
+                <br className="br_mo" /> raise GitHub
+                <br className="br_tablet" /> discussions.
+              </Text>
+              <Flex gap="6" justifyContent={{ base: 'space-evenly', lg: 'flex-start' }}>
+                <Button
+                  icon={<DiscordIcon />}
+                  href="https://discord.gg/MVEAwz9sBy"
+                  as="link"
+                  variant="outline"
+                  position="start"
+                  size="lg"
+                >
+                  Discord
+                </Button>
+                <Button
+                  icon={<GitHubIcon />}
+                  href="https://github.com/yorkie-team/dashboard/issues"
+                  as="link"
+                  variant="outline"
+                  position="start"
+                  size="lg"
+                >
+                  GitHub
+                </Button>
+              </Flex>
+            </Box>
+            <Box paddingBlock={{ base: '12', lg: '0' }}>
+              <CommunitySVG />
+            </Box>
+          </Flex>
         </Flex>
       </Container>
     </Layout>
