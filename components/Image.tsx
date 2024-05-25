f8a4802b70f3afaac08ca6a5216158c4eedbb44d
Vi Tran ( Alice) <vi.tran@navercorp.com>
2024-05-25 22:05:25 +0700

    update docs

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,16 +1,18 @@
+import React from 'react';
 import { ImageProps as NextImageProps, StaticImageData } from 'next/image';
-import ExportedImage from 'next-image-export-optimizer';
+import dynamic from 'next/dynamic';
 import { prefix } from '@/utils/prefix';
 
 export type ImageProps = {
   src: string | StaticImageData;
 } & Omit<NextImageProps, 'src'>;
 
+// Use dynamic import for the ExportedImage component
+const ExportedImage = dynamic(() => import('next-image-export-optimizer'), { ssr: false });
+
 export function Image({ src, alt, width, height, ...rest }: ImageProps) {
   const isInternalLink = typeof src === 'string' && src.startsWith('/');
 
-  // TODO(chacha912): Fix the 404 error that occurs for image files
-  // because `next export` is not performed in the development environment.
   return (
     <ExportedImage src={isInternalLink ? `${prefix}${src}` : src} alt={alt} width={width} height={height} {...rest} />
   );
