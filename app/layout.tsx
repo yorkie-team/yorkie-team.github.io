e0ed9220145eeb023cb1315ca2c98daa5e348476
DongjaJ <jdh981118@naver.com>
2024-12-16 11:47:07 +0900

    chore: migration pages/_app.tsx to app/layout.tsx, app/theme-initializer.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,66 @@
+import { prefix } from '@/utils/prefix';
+import Script from 'next/script';
+import ThemeInitializer from './_components/theme-initializer';
+import '@/styles/style.css';
+import { Metadata, Viewport } from 'next';
+
+export const metadata: Metadata = {
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL!),
+  icons: {
+    icon: [
+      { url: `${prefix}/favicon.ico` },
+      { url: `${prefix}/favicon-16x16.png`, type: 'image/png', sizes: '16x16' },
+      {
+        url: `${prefix}/favicon-32x32.png`,
+        type: 'image/png',
+        sizes: '32x32',
+      },
+    ],
+    apple: [{ url: `${prefix}/favicon-512x512.png` }],
+  },
+  description: 'Easily add collaboration to your apps with our API-based services.',
+  openGraph: {
+    type: 'website',
+    siteName: 'Yorkie',
+    title: 'Yorkie - Bring collaboration to your app',
+    images: [{ url: `/og.png` }],
+    url: '',
+    description: 'Easily add collaboration to your apps with our API-based services.',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@team_yorkie',
+    title: 'Yorkie - Bring collaboration to your app',
+    images: [`/og.png`],
+    description: 'Easily add collaboration to your apps with our API-based services.',
+  },
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  viewportFit: 'cover',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-7KXWLDH8CH" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-7KXWLDH8CH');
+        `}
+        </Script>
+      </head>
+      <body>
+        <ThemeInitializer />
+        {children}
+      </body>
+    </html>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,52 +1,8 @@
-import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
-import Script from 'next/script';
-import '@/styles/style.scss';
-import { prefix } from '@/utils/prefix';
-import { ThemeOption, useTheme } from '@/hooks/useTheme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const { setTheme } = useTheme();
-  useEffect(() => {
-    const themeOption = (window.localStorage.getItem('theme') || 'system') as ThemeOption;
-    setTheme(themeOption);
-  }, [setTheme]);
-
   return (
     <>
-      <Head>
-        <link rel="icon" href={`${prefix}/favicon.ico`} />
-        <link rel="icon" type="image/png" sizes="16x16" href={`${prefix}/favicon-16x16.png`} />
-        <link rel="icon" type="image/png" sizes="32x32" href={`${prefix}/favicon-32x32.png`} />
-        <link rel="apple-touch-icon" href={`${prefix}/favicon-512x512.png`} />
-        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
-        <meta name="description" content="Easily add collaboration to your apps with our API-based services." />
-        <meta property="og:type" content="website" />
-        <meta property="og:site_name" content="Yorkie" />
-        <meta property="og:title" content="Yorkie - Bring collaboration to your app" />
-        <meta property="og:image" content={`${process.env.NEXT_PUBLIC_SITE_URL}/og.png`} />
-        <meta property="og:url" content={process.env.NEXT_PUBLIC_SITE_URL} />
-        <meta property="og:description" content="Easily add collaboration to your apps with our API-based services." />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@team_yorkie" />
-        <meta property="twitter:title" content="Yorkie - Bring collaboration to your app" />
-        <meta property="twitter:image" content={`${process.env.NEXT_PUBLIC_SITE_URL}/og.png`} />
-        <meta
-          property="twitter:description"
-          content="Easily add collaboration to your apps with our API-based services."
-        />
-      </Head>
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-7KXWLDH8CH" strategy="afterInteractive" />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-7KXWLDH8CH');
-        `}
-      </Script>
       <Component {...pageProps} />
     </>
   );
