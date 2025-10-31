fd62569ddea98aac2ea84dd5c13a7aba96f720ac
Youngteac Hong <susukang98@gmail.com>
2025-10-31 14:30:50 +0900

    Serve example apps directly

--- components/exampleView/const.ts
+++ components/exampleView/const.ts
@@ -1,2 +1,2 @@
 export const EXAMPLE_CODE_URL = 'https://github.com/yorkie-team/yorkie-js-sdk/tree/main/examples/';
-export const EXAMPLE_PREVIEW_URL = 'https://yorkie.dev/yorkie-js-sdk/examples/';
+export const EXAMPLE_PREVIEW_URL = '/apps/';
